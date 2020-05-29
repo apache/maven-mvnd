ea6079b06b87b385b337c9ea9f1414f234337841
Peter Palaga <ppalaga@redhat.com>
2020-05-29 10:45:21 +0200

    Run integration tests with an isolated Mavel local repo and settings.xml

--- integration-tests/src/test/java/org/jboss/fuse/mvnd/junit/MvndTestExtension.java
+++ integration-tests/src/test/java/org/jboss/fuse/mvnd/junit/MvndTestExtension.java
@@ -2,17 +2,21 @@ package org.jboss.fuse.mvnd.junit;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.jboss.fuse.mvnd.daemon.Client;
 import org.jboss.fuse.mvnd.daemon.DaemonInfo;
 import org.jboss.fuse.mvnd.daemon.DaemonRegistry;
 import org.jboss.fuse.mvnd.daemon.Layout;
+import org.jboss.fuse.mvnd.daemon.ClientLayout;
 import org.jboss.fuse.mvnd.jpm.ProcessImpl;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
@@ -23,6 +27,7 @@ import org.junit.jupiter.api.extension.ExtensionContext.Store;
 public class MvndTestExtension implements BeforeAllCallback, BeforeEachCallback, AfterAllCallback {
 
     private volatile Exception bootException;
+
     public MvndTestExtension() {
         super();
     }
@@ -33,7 +38,8 @@ public class MvndTestExtension implements BeforeAllCallback, BeforeEachCallback,
             final Store store = context.getRoot().getStore(ExtensionContext.Namespace.GLOBAL);
             final Class<?> testClass = context.getRequiredTestClass();
             final MvndTest mnvdTest = testClass.getAnnotation(MvndTest.class);
-            store.put(MvndResource.class.getName(), MvndResource.create(context.getRequiredTestClass().getSimpleName(), mnvdTest.projectDir()));
+            store.put(MvndResource.class.getName(),
+                    MvndResource.create(context.getRequiredTestClass().getSimpleName(), mnvdTest.projectDir()));
         } catch (Exception e) {
             this.bootException = e;
         }
@@ -59,7 +65,9 @@ public class MvndTestExtension implements BeforeAllCallback, BeforeEachCallback,
                     } else if (f.getType() == Layout.class) {
                         f.set(testInstance, resource.layout);
                     } else if (f.getType() == Client.class) {
-                        f.set(testInstance, new Client(resource.layout));
+                        f.set(testInstance, new Client(resource.layout, Optional.of(resource.clientLayout)));
+                    } else if (f.getType() == ClientLayout.class) {
+                        f.set(testInstance, resource.clientLayout);
                     }
                 }
             }
@@ -78,6 +86,7 @@ public class MvndTestExtension implements BeforeAllCallback, BeforeEachCallback,
 
     static class MvndResource implements ExtensionContext.Store.CloseableResource {
 
+        private final ClientLayout clientLayout;
         private final Layout layout;
         private final DaemonRegistry registry;
 
@@ -87,13 +96,15 @@ public class MvndTestExtension implements BeforeAllCallback, BeforeEachCallback,
             }
             final Path mvndTestSrcDir = Paths.get(rawProjectDir).toAbsolutePath().normalize();
             if (!Files.exists(mvndTestSrcDir)) {
-                throw new IllegalStateException("@MvndTest(projectDir = \""+ rawProjectDir +"\") points at a path that does not exist: " + mvndTestSrcDir);
+                throw new IllegalStateException("@MvndTest(projectDir = \"" + rawProjectDir
+                        + "\") points at a path that does not exist: " + mvndTestSrcDir);
             }
 
             final Path testDir = Paths.get("target/mvnd-tests/" + className).toAbsolutePath();
+            final Path testExecutionDir = testDir.resolve("project");
             try (Stream<Path> files = Files.walk(mvndTestSrcDir)) {
                 files.forEach(source -> {
-                    final Path dest = testDir.resolve(mvndTestSrcDir.relativize(source));
+                    final Path dest = testExecutionDir.resolve(mvndTestSrcDir.relativize(source));
                     try {
                         if (Files.isDirectory(source)) {
                             Files.createDirectories(dest);
@@ -107,22 +118,67 @@ public class MvndTestExtension implements BeforeAllCallback, BeforeEachCallback,
                 });
             }
 
-            final Path mvndHome = Paths.get(Objects.requireNonNull(System.getProperty("mvnd.home"), "System property mvnd.home must be set")).normalize().toAbsolutePath();
+            final Path mvndHome = Paths
+                    .get(Objects.requireNonNull(System.getProperty("mvnd.home"), "System property mvnd.home must be set"))
+                    .normalize().toAbsolutePath();
             if (!Files.isDirectory(mvndHome)) {
-                throw new IllegalStateException("The value of mvnd.home system property points at a path that does not exist or is not a directory");
+                throw new IllegalStateException(
+                        "The value of mvnd.home system property points at a path that does not exist or is not a directory");
             }
             final Layout layout = new Layout(Paths.get(System.getProperty("java.home")).toAbsolutePath().normalize(),
                     mvndHome,
-                    testDir,
-                    testDir);
+                    testExecutionDir,
+                    testExecutionDir);
             final DaemonRegistry registry = new DaemonRegistry(layout.registry());
-            return new MvndResource(layout, registry);
+
+            final Path localMavenRepository = deleteDir(testDir.resolve("local-maven-repo"));
+            final Path settingsPath = createSettings(testDir.resolve("settings.xml"));
+            final ClientLayout clientLayout = new ClientLayout(localMavenRepository, settingsPath);
+            return new MvndResource(layout, registry, clientLayout);
         }
 
-        public MvndResource(Layout layout, DaemonRegistry registry) {
+        static Path deleteDir(Path dir) {
+            if (Files.exists(dir)) {
+                try (Stream<Path> files = Files.walk(dir)) {
+                    files.sorted(Comparator.reverseOrder())
+                            .forEach(f -> {
+                                try {
+                                    Files.delete(f);
+                                } catch (IOException e) {
+                                    throw new RuntimeException("Could not delete " + f);
+                                }
+                            });
+                } catch (IOException e1) {
+                    throw new RuntimeException("Could not walk " + dir);
+                }
+            }
+            return dir;
+        }
+
+        static Path createSettings(Path settingsPath) {
+            final Path settingsTemplatePath = Paths.get("src/test/resources/settings-template.xml");
+            try {
+                final String template = new String(Files.readAllBytes(settingsTemplatePath), StandardCharsets.UTF_8);
+                /* mrm.repository.url is set by mrm-maven-plugin */
+                final String mrmsRepoUrl = Objects.requireNonNull(System.getProperty("mrm.repository.url"),
+                        "System property mrm.repository.url");
+                final String content = template.replace("${mrm.repository.url}", mrmsRepoUrl);
+                try {
+                    Files.write(settingsPath, content.getBytes(StandardCharsets.UTF_8));
+                } catch (Exception e) {
+                    throw new RuntimeException("Could not write " + settingsPath);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Could not read " + settingsTemplatePath);
+            }
+            return settingsPath;
+        }
+
+        public MvndResource(Layout layout, DaemonRegistry registry, ClientLayout clientLayout) {
             super();
             this.layout = layout;
             this.registry = registry;
+            this.clientLayout = clientLayout;
         }
 
         @Override
