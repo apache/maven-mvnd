a8c302fb9ffcba4de799e0418d8737eef23f0e65
Guillaume Nodet <gnodet@gmail.com>
2020-10-09 11:58:00 +0200

    Fix license

--- daemon/src/main/java/org/apache/maven/cli/logging/Slf4jLogger.java
+++ daemon/src/main/java/org/apache/maven/cli/logging/Slf4jLogger.java
@@ -33,24 +33,6 @@ package org.apache.maven.cli.logging;
  * specific language governing permissions and limitations
  * under the License.
  */
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 import org.codehaus.plexus.logging.Logger;
 import org.jboss.fuse.mvnd.logging.smart.ProjectBuildLogAppender;
 import org.slf4j.MDC;
@@ -59,8 +41,10 @@ import org.slf4j.MDC;
  * Adapt an SLF4J logger to a Plexus logger, ignoring Plexus logger API parts that are not classical and
  * probably not really used.
  *
+ * <p>
+ * Adapted from https://github.com/apache/maven/blob/maven-3.6.3/maven-embedder/src/main/java/org/apache/maven/cli/logging/Slf4jLogger.java
+ *
  * @author Jason van Zyl
- * @since  3.1.0
  */
 public class Slf4jLogger
         implements Logger {
