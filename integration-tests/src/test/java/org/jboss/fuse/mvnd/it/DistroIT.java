8f322e7b0ba05dfd7e8037fcb8e0ced58f7a21ce
Peter Palaga <ppalaga@redhat.com>
2020-10-23 18:21:23 +0200

    Split daemon module into daemon and dist #130

--- integration-tests/src/test/java/org/jboss/fuse/mvnd/it/DistroIT.java
+++ integration-tests/src/test/java/org/jboss/fuse/mvnd/it/DistroIT.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.fuse.mvnd.dist;
+package org.jboss.fuse.mvnd.it;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
