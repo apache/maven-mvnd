575de9aa9aab1040c27282b20a1979fec432bb7e
Peter Palaga <ppalaga@redhat.com>
2020-11-13 23:42:18 +0100

    Reproduce #218 pom.xml changes not honored

---
+++ integration-tests/src/test/java/org/mvndaemon/mvnd/it/NewManagedModuleNativeIT.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mvndaemon.mvnd.it;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.inject.Inject;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mvndaemon.mvnd.assertj.TestClientOutput;
+import org.mvndaemon.mvnd.client.Client;
+import org.mvndaemon.mvnd.common.DaemonInfo;
+import org.mvndaemon.mvnd.junit.ClientFactory;
+import org.mvndaemon.mvnd.junit.MvndNativeTest;
+import org.mvndaemon.mvnd.junit.TestParameters;
+import org.mvndaemon.mvnd.junit.TestRegistry;
+
+@MvndNativeTest(projectDir = "src/test/projects/new-managed-module")
+public class NewManagedModuleNativeIT {
+
+    @Inject
+    TestParameters parameters;
+
+    @Inject
+    TestRegistry registry;
+
+    @Inject
+    ClientFactory clientFactory;
+
+    @Test
+    void upgrade() throws IOException, InterruptedException {
+        Assertions.assertThat(registry.getAll().size()).isEqualTo(0);
+
+        /* Build the initial state of the test project */
+        final Path parentDir = parameters.getTestDir().resolve("project/parent");
+        final Client cl = clientFactory.newClient(parameters.cd(parentDir));
+        {
+            final TestClientOutput output = new TestClientOutput();
+            cl.execute(output, "clean", "install", "-e", "-B", "-ntp").assertSuccess();
+        }
+        Assertions.assertThat(registry.getAll().size()).isEqualTo(1);
+
+        final DaemonInfo d = registry.getAll().get(0);
+        /* Wait, till the instance becomes idle */
+        registry.awaitIdle(d.getUid());
+
+        /* Do the changes */
+        final Path srcDir = parentDir.resolve("../changes").normalize();
+        try (Stream<Path> files = Files.walk(srcDir)) {
+            files.forEach(source -> {
+                final Path relPath = srcDir.relativize(source);
+                final Path dest = parentDir.resolve(relPath);
+                try {
+                    if (Files.isDirectory(source)) {
+                        Files.createDirectories(dest);
+                    } else {
+                        Files.createDirectories(dest.getParent());
+                        Files.copy(source, dest, StandardCopyOption.REPLACE_EXISTING);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        /* Build again */
+        {
+            final TestClientOutput output = new TestClientOutput();
+            cl.execute(output, "clean", "install", "-e", "-B", "-ntp").assertSuccess();
+
+            final List<String> messagesToString = output.messagesToString();
+            Assertions.assertThat(messagesToString.stream().noneMatch(m -> m.contains("[ERROR]")))
+                    .withFailMessage("Should contain no errors:\n    %s",
+                            messagesToString.stream().collect(Collectors.joining("\n    ")))
+                    .isTrue();
+        }
+        Assertions.assertThat(registry.getAll().size()).isEqualTo(1);
+
+    }
+}
