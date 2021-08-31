c95ae1d0122d8129fff093d13a2ad8ec4b8ac209
Guillaume Nodet <gnodet@gmail.com>
2021-08-31 11:27:18 +0200

    Provide an eviction pattern to get rid of classloaders for bad behaving plugins, #440 (#448)

--- common/src/main/java/org/mvndaemon/mvnd/common/Environment.java
+++ common/src/main/java/org/mvndaemon/mvnd/common/Environment.java
@@ -243,7 +243,11 @@ public enum Environment {
     /**
      * Socket family to use
      */
-    MVND_SOCKET_FAMILY("mvnd.socketFamily", null, "inet", OptionType.STRING, Flags.DISCRIMINATING);
+    MVND_SOCKET_FAMILY("mvnd.socketFamily", null, "inet", OptionType.STRING, Flags.DISCRIMINATING),
+    /**
+     * Regexp pattern that will force eviction of the plugin realms if one of its dependencies matches.
+     */
+    MVND_PLUGIN_REALM_EVICT_PATTERN("mvnd.pluginRealmEvictPattern", null, "", OptionType.STRING, Flags.OPTIONAL);
 
     static Properties properties;
 
