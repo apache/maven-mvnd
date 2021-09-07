da714c98ac1eef21ded81f27540e52477fde499d
Guillaume Nodet <gnodet@gmail.com>
2021-09-07 16:25:34 +0200

    Speed improvement (MNG-7235)

--- daemon/src/main/java/org/apache/maven/graph/DefaultProjectDependencyGraph.java
+++ daemon/src/main/java/org/apache/maven/graph/DefaultProjectDependencyGraph.java
@@ -36,10 +36,14 @@ package org.apache.maven.graph;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.apache.maven.execution.ProjectDependencyGraph;
 import org.apache.maven.project.DuplicateProjectException;
 import org.apache.maven.project.MavenProject;
@@ -58,6 +62,10 @@ public class DefaultProjectDependencyGraph
 
     private List<MavenProject> allProjects;
 
+    private Map<String, Integer> order;
+
+    private Map<String, MavenProject> projects;
+
     /**
      * Creates a new project dependency graph based on the specified projects.
      *
@@ -70,6 +78,15 @@ public class DefaultProjectDependencyGraph
         super();
         this.allProjects = Collections.unmodifiableList(new ArrayList<>(projects));
         this.sorter = new ProjectSorter(projects);
+        this.order = new HashMap<>();
+        this.projects = new HashMap<>();
+        List<MavenProject> sorted = this.sorter.getSortedProjects();
+        for (int index = 0; index < sorted.size(); index++) {
+            MavenProject project = sorted.get(index);
+            String id = ProjectSorter.getId(project);
+            this.projects.put(id, project);
+            this.order.put(id, index);
+        }
     }
 
     /**
@@ -88,6 +105,15 @@ public class DefaultProjectDependencyGraph
         super();
         this.allProjects = Collections.unmodifiableList(new ArrayList<>(allProjects));
         this.sorter = new ProjectSorter(projects);
+        this.order = new HashMap<>();
+        this.projects = new HashMap<>();
+        List<MavenProject> sorted = this.sorter.getSortedProjects();
+        for (int index = 0; index < sorted.size(); index++) {
+            MavenProject project = sorted.get(index);
+            String id = ProjectSorter.getId(project);
+            this.projects.put(id, project);
+            this.order.put(id, index);
+        }
     }
 
     /**
@@ -138,15 +164,10 @@ public class DefaultProjectDependencyGraph
     }
 
     private List<MavenProject> getSortedProjects(Set<String> projectIds) {
-        List<MavenProject> result = new ArrayList<>(projectIds.size());
-
-        for (MavenProject mavenProject : sorter.getSortedProjects()) {
-            if (projectIds.contains(ProjectSorter.getId(mavenProject))) {
-                result.add(mavenProject);
-            }
-        }
-
-        return result;
+        return projectIds.stream()
+                .sorted(Comparator.comparing(order::get))
+                .map(projects::get)
+                .collect(Collectors.toList());
     }
 
     @Override
