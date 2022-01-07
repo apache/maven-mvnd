b3a55333a8c5dd4703d6646e2a402d803eefee0b
Guillaume Nodet <gnodet@gmail.com>
2022-01-07 11:46:54 +0100

    Fix NoSuchFileException when using the compile phase, fixes #564

---
+++ integration-tests/src/test/java/org/mvndaemon/mvnd/it/AttachedArtifactResolutionTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mvndaemon.mvnd.it;
+
+import javax.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.mvndaemon.mvnd.assertj.TestClientOutput;
+import org.mvndaemon.mvnd.client.Client;
+import org.mvndaemon.mvnd.junit.MvndTest;
+
+@MvndTest(projectDir = "src/test/projects/attached-artifact-resolution")
+public class AttachedArtifactResolutionTest {
+
+    @Inject
+    Client client;
+
+    @Test
+    void cleanCompile() throws Exception {
+        final TestClientOutput output = new TestClientOutput();
+        client.execute(output, "clean", "compile", "-e", "-Dmvnd.log.level=DEBUG")
+                .assertSuccess();
+    }
+
+}
