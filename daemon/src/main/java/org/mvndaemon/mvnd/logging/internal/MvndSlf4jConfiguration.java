3b100839a810ce9ae2555c1592b5edb623223924
Guillaume Nodet <gnodet@gmail.com>
2022-03-05 09:03:14 +0100

    Fix mvn/bin/mvn debug output (#606)

--- daemon/src/main/java/org/mvndaemon/mvnd/logging/internal/MvndSlf4jConfiguration.java
+++ daemon/src/main/java/org/mvndaemon/mvnd/logging/internal/MvndSlf4jConfiguration.java
@@ -16,10 +16,27 @@
 package org.mvndaemon.mvnd.logging.internal;
 
 import org.apache.maven.cli.logging.Slf4jConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MvndSlf4jConfiguration implements Slf4jConfiguration {
     @Override
     public void setRootLoggerLevel(Level level) {
+        ch.qos.logback.classic.Level value;
+        switch (level) {
+        case DEBUG:
+            value = ch.qos.logback.classic.Level.DEBUG;
+            break;
+
+        case INFO:
+            value = ch.qos.logback.classic.Level.INFO;
+            break;
+
+        default:
+            value = ch.qos.logback.classic.Level.ERROR;
+            break;
+        }
+        ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)).setLevel(value);
     }
 
     @Override
