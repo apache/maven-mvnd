92cbc7d6cf8af1822fa1912a3f4f737dd698685e
Tamas Cservenak <tamas@cservenak.net>
2022-12-15 19:26:58 +0100

    Update Maven to 4.0.0-alpha-3 and Maven Resolver to 1.9.2 (#742)
    https://issues.apache.org/jira/browse/MNG-7586
    
    * Remove CliMavenPluginManager which has the changed needed in alpha-3
    * Align slf4j api with maven
    * Make sure the invoker being called from IT reuses the settings from the invoker running the IT
    * Fix IT when mrm is disabled
    * Fix InvalidingPluginRealmCache
    
    Co-authored-by: Guillaume Nodet <gnodet@gmail.com>

---
+++ daemon/src/main/java/org/mvndaemon/mvnd/syncontext/DaemonNamedLockFactoryAdapterFactoryImpl.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.mvndaemon.mvnd.syncontext;
+
+import java.util.Map;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import org.eclipse.aether.impl.RepositorySystemLifecycle;
+import org.eclipse.aether.internal.impl.synccontext.named.NameMapper;
+import org.eclipse.aether.internal.impl.synccontext.named.NamedLockFactoryAdapterFactoryImpl;
+import org.eclipse.aether.internal.impl.synccontext.named.providers.FileGAVNameMapperProvider;
+import org.eclipse.aether.named.NamedLockFactory;
+import org.eclipse.aether.named.providers.FileLockNamedLockFactory;
+import org.eclipse.sisu.Priority;
+
+/**
+ * Mvnd named lock factory implementation: it differs from
+ * {@link org.eclipse.aether.internal.impl.synccontext.named.NamedLockFactoryAdapterFactoryImpl} only by default values.
+ */
+@Singleton
+@Named
+@Priority(10)
+public final class DaemonNamedLockFactoryAdapterFactoryImpl extends NamedLockFactoryAdapterFactoryImpl {
+    @Inject
+    public DaemonNamedLockFactoryAdapterFactoryImpl(
+            final Map<String, NamedLockFactory> factories,
+            final Map<String, NameMapper> nameMappers,
+            final RepositorySystemLifecycle lifecycle) {
+        super(factories, FileLockNamedLockFactory.NAME, nameMappers, FileGAVNameMapperProvider.NAME, lifecycle);
+    }
+}
