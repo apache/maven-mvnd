b17ae5997b4556d8dd9946246dc6feaa81f0e020
Guillaume Nodet <gnodet@gmail.com>
2023-03-07 16:57:07 +0100

    Fix formatting

--- daemon/src/main/java/org/mvndaemon/mvnd/cli/EnvHelper.java
+++ daemon/src/main/java/org/mvndaemon/mvnd/cli/EnvHelper.java
@@ -114,8 +114,7 @@ public class EnvHelper {
         // change current dir for the java.io.File class
         Class<?> fileClass = Class.forName("java.io.File");
         if (JavaVersion.getJavaSpec() >= 11.0) {
-            Field fsField = fileClass.getDeclaredField(
-                    JavaVersion.getJavaSpec() >= 21.0 ? "FS" : "fs");
+            Field fsField = fileClass.getDeclaredField(JavaVersion.getJavaSpec() >= 21.0 ? "FS" : "fs");
             fsField.setAccessible(true);
             Object fs = fsField.get(null);
             Field userDirField = fs.getClass().getDeclaredField("userDir");
