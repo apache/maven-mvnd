109c9de44e048f052f30ed0c705eaa1cffa248c8
Tamas Cservenak <tamas@cservenak.net>
2024-04-20 13:24:54 +0200

    Default must be manually handled, i missed this.
    Fixes #912

---
+++ integration-tests/src/test/java/org/mvndaemon/mvnd/it/MavenConfIgnoreExtNativeIT.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.mvndaemon.mvnd.it;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mvndaemon.mvnd.junit.MvndNativeTest;
+
+@MvndNativeTest(projectDir = "src/test/projects/maven-conf-ignore-ext")
+class MavenConfIgnoreExtNativeIT extends MavenConfNativeIT {
+    @Override
+    protected List<String> mvndParams() {
+        ArrayList<String> result = new ArrayList<>(super.mvndParams());
+        result.add("-Dmvnd.coreExtensionsExclude=foo:bar");
+        return result;
+    }
+}
