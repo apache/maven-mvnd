808be19c76d20c26b5acee55dc25cbbdd8182f42
Guillaume Nodet <gnodet@gmail.com>
2024-06-20 13:16:28 +0200

    Revert "Add Diag with more info for exceptions"
    This reverts commit 1508fe5df78686c89d0f1576fcacf542081473f0.

--- client/src/main/java-mvnd/org/mvndaemon/mvnd/client/DefaultClient.java
+++ client/src/main/java-mvnd/org/mvndaemon/mvnd/client/DefaultClient.java
@@ -87,7 +87,7 @@ public class DefaultClient implements Client {
 
         // Diag
         if (Environment.DIAG.removeCommandLineOption(args) != null) {
-            Diag.diag(System.out);
+            org.jline.terminal.impl.Diag.diag(System.out);
             return;
         }
 
