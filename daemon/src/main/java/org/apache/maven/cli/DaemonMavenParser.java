80562675f9de2b3d5e1fba94df6f5ec18cca80fe
Tamas Cservenak <tamas@cservenak.net>
2024-10-17 11:54:44 +0200

    Revert "Experiment No2: force HTTP/1.1 on JDK transport"
    This reverts commit 826af6b55604cbec0d653be883fec781cc7cf53c.

--- daemon/src/main/java/org/apache/maven/cli/DaemonMavenParser.java
+++ daemon/src/main/java/org/apache/maven/cli/DaemonMavenParser.java
@@ -81,7 +81,7 @@ public class DaemonMavenParser extends BaseMavenParser<MavenOptions, MavenInvoke
     @Override
     protected Map<String, String> populateUserProperties(LocalContext context) throws ParserException, IOException {
         HashMap<String, String> props = new HashMap<>(super.populateUserProperties(context));
-        props.put("aether.transport.jdk.httpVersion", "HTTP_1_1");
+        props.put("maven.resolver.transport", "apache");
         return props;
     }
 
