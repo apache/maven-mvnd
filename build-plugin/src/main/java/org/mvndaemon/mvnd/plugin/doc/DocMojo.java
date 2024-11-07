e2c9e47a8030404f3c7152d4a83b78fe854d5793
Guillaume Nodet <gnodet@gmail.com>
2024-11-07 18:20:37 +0100

    Upgrade to Maven 4.0.0-beta-5

--- build-plugin/src/main/java/org/mvndaemon/mvnd/plugin/doc/DocMojo.java
+++ build-plugin/src/main/java/org/mvndaemon/mvnd/plugin/doc/DocMojo.java
@@ -37,7 +37,6 @@ import java.util.TreeSet;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -73,10 +72,10 @@ public class DocMojo extends AbstractMojo {
     boolean skip;
 
     @Override
+    @SuppressWarnings("deprecation")
     public void execute() throws MojoExecutionException, MojoFailureException {
-        final Log log = getLog();
         if (skip) {
-            log.info(getClass().getSimpleName() + " skipped per skip parameter");
+            getLog().info(getClass().getSimpleName() + " skipped per skip parameter");
             return;
         }
 
